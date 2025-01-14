1f675cc8140f7de06703f3395b4e50c1eb795b9c
Mr. Devansh Shah <devansh.shah.de@gmail.com>
2025-01-14 16:26:30 +0530

    Initial Commit

---
+++ CIVICA_Python_Trainings/Day1.py
@@ -0,0 +1,64 @@
+import keyword
+
+class First_Day:
+    
+# first.py 
+    print("Hello World")
+
+# Defining Variables
+name = "Devansh Shah"
+age = 20
+height = 5.9
+nameStartsWith = "D"
+isStudent = True
+
+# Printing Variables
+print(name)
+print(age)
+print(height)
+print(nameStartsWith)
+print(isStudent)
+
+# Taking Input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+nameStartsWith = input("Enter the first letter of your name: ")
+isStudent = input("Are you a student? ")
+
+# Again Printing Variables
+print(name)
+print(age)
+print(height)
+print(nameStartsWith)
+print(isStudent)
+
+# Handling Identifiers
+def show_user_age():
+    userAge = 25
+
+# Handling Keyword 
+print(keyword.kwlist)
+
+# Handling Comments
+# Use # For Single Line Comment 
+
+'''
+
+Use tripple coma for multi line comments.
+
+'''
+
+# Handling Escape Character
+# Some special characters are not allowed in a string, If you want to use them you can use it as an escape character
+
+'''
+\n - New Line
+
+\t - Tab
+
+\" - Double Quote
+
+\\ - Path
+'''
+_______
---
+++ CIVICA_Python_Trainings/Day2.py
@@ -0,0 +1,50 @@
+
+# Lesson on Variables in Python
+
+# Variables are used to store data that can be used later in the program.
+# In Python, you don't need to declare the type of variable, you just assign a value to it.
+
+# Example of different types of variables:
+
+# Integer
+age = 25
+print("Age:", age)
+
+# Float
+height = 5.9
+print("Height:", height)
+
+# String
+name = "John Doe"
+print("Name:", name)
+
+# Boolean
+is_student = True
+print("Is Student:", is_student)
+
+# You can also change the value of a variable after it has been assigned
+age = 26
+print("New Age:", age)
+
+# Variables can be used in expressions
+years_to_graduate = 4
+graduation_age = age + years_to_graduate
+print("Graduation Age:", graduation_age)
+
+# You can also assign multiple variables in one line
+x, y, z = 1, 2, 3
+print("x:", x, "y:", y, "z:", z)
+
+a,b,c,= 4,5,6
+print("a:", a, "b:", b, "c:", c)
+
+# Variables can be reassigned to different types
+x = "Now I'm a string"
+print("x:", x)
+
+# Remember, variable names should be descriptive and follow the naming conventions
+# They should start with a letter or underscore, and can contain letters, numbers, and underscores
+# Examples of valid variable names: my_variable, variable1, _variable
+# Examples of invalid variable names: 1variable, variable-name
+
+# That's it for the basics of variables in Python!
